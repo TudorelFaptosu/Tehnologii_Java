d6e1ba93f9c74e14c3a5037347ead890ce8c288f
TudorelFaptosu <tudor.caldarescu@gmail.com>
2025-12-11 05:53:58 +0200

    lab4 homework done plus part of 5

--- Lab4/demo/src/main/resources/db/migration/V1__Create_Schema.sql
+++ Lab4/demo/src/main/resources/db/migration/V1__Create_Schema.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE instructors (
-                             id SERIAL PRIMARY KEY,
+                             id BIGSERIAL PRIMARY KEY,
                              name VARCHAR(100) NOT NULL,
                              email VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE students (
-                          id SERIAL PRIMARY KEY,
+                          id BIGSERIAL PRIMARY KEY,
                           code VARCHAR(50) UNIQUE NOT NULL,
                           name VARCHAR(100) NOT NULL,
                           email VARCHAR(100) UNIQUE NOT NULL,
@@ -14,20 +14,28 @@ CREATE TABLE students (
 );
 
 CREATE TABLE packs (
-                       id SERIAL PRIMARY KEY,
+                       id BIGSERIAL PRIMARY KEY,
                        study_year INT NOT NULL,
                        semester INT NOT NULL,
                        name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE courses (
-                         id SERIAL PRIMARY KEY,
+                         id BIGSERIAL PRIMARY KEY,
                          type VARCHAR(20) NOT NULL, -- 'OPTIONAL' or 'COMPULSORY'
                          code VARCHAR(20) UNIQUE NOT NULL,
                          abbr VARCHAR(10),
                          name VARCHAR(150) NOT NULL,
                          description TEXT,
-                         group_count INT DEFAULT 1,
-                         instructor_id INT REFERENCES instructors(id),
-                         pack_id INT REFERENCES packs(id)
+                         group_count INTEGER DEFAULT 1,
+                         instructor_id BIGINT REFERENCES instructors(id),
+                         pack_id BIGINT REFERENCES packs(id)
+);
+CREATE TABLE preferences (
+                             id BIGSERIAL PRIMARY KEY,
+                             student_id BIGINT NOT NULL REFERENCES students(id),
+                             course_id BIGINT NOT NULL REFERENCES courses(id),
+                             rank INTEGER NOT NULL,
+    -- Constrângerea unică din Java: un student nu poate vota același curs de două ori
+                             UNIQUE (student_id, course_id)
 );
