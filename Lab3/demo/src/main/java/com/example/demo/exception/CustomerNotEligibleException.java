5e46f11479c75ed11087fd379759ec903f061e49
TudorelFaptosu <tudor.caldarescu@gmail.com>
2026-01-15 13:20:42 +0200

    refactor lab

--- Lab3/demo/src/main/java/com/example/demo/exception/CustomerNotEligibleException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class CustomerNotEligibleException extends RuntimeException
-{
-    public CustomerNotEligibleException(String message)
-    {
-        super(message);
-    }
-}
--- Lab3/demo/src/main/java/com/example/demo/service/DiscountService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.domain.Order;
-
-public interface DiscountService {
-    double applyDiscount(Order order);
-
-}
